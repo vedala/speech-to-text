ff1dfae47edb6a2fc896455b8d6762bf2005af77
Kalyan Vedala <svedala23@gmail.com>
2025-09-29 05:24:38 -0700

    Copy over app files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import RecordUploadAudio from './RecordUploadAudio';
 
 function App() {
+  navigator.mediaDevices.getUserMedia({ audio: true })
+  .then(stream => {
+    // got audio stream
+  })
+  .catch(err => {
+    console.error("Microphone access denied:", err);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>STT</h1>
+      <RecordUploadAudio />
     </div>
   );
 }
